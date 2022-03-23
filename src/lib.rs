d2b95defaffffa02684a6f2acad446be2b1db0ae
Justin Latimer <justinlatimer@gmail.com>
2022-03-23 20:21:32 +1300

    Start modelling config.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,60 @@
+#![no_std]
+
+use embedded_hal::blocking::i2c;
+
+#[repr(u8)]
+enum Register {
+    Configuration = 0x00,
+    ShuntVoltage = 0x01,
+    BusVoltage = 0x02,
+    Power = 0x03,
+    Current = 0x04,
+    Callibration = 0x05,
+    MaskEnable = 0x06,
+    AlertLimit = 0x07,
+    ManufacturerID = 0xFE,
+    DieID = 0xFF,
+}
+
+enum AVG {
+    _1 = 0b000,
+    _4 = 0b001,
+    _16 = 0b010,
+    _64 = 0b011,
+    _128 = 0b100,
+    _256 = 0b101,
+    _512 = 0b110,
+    _1024 = 0b111,
+}
+
+enum VBUSCT {
+    _140us = 0b000,
+    _204us = 0b001,
+    _332us = 0b010,
+    _588us = 0b011,
+    _1100us = 0b100,
+    _2116us = 0b101,
+    _4156us = 0b110,
+    _8244us = 0b111,
+}
+
+const ADDRESS_MIN: u8 = 0b1000000;
+const ADDRESS_MAX: u8 = 0b1001111;
+
+pub struct INA226<I2C> {
+    i2c: I2C,
+    address: u8,
+}
+
+impl<I2C, E> INA226<I2C>
+where
+    I2C: i2c::Write<Error = E> + i2c::Read<Error = E>,
+{
+    pub fn new(i2c: I2C, address: u8) -> INA226<I2C> {
+        INA226 { i2c, address }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
